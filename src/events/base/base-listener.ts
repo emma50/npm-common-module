b42f4d28bcd97ecf503cdda1383a2777e26e2f8e
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-09-15 08:41:05 +0100

    Updates

---
+++ src/events/base/base-listener.ts
@@ -0,0 +1,55 @@
+import { Message, Stan } from 'node-nats-streaming'
+import { Subjects } from '../subjects'
+
+interface Event {
+  subject: Subjects,
+  data: any,
+}
+export abstract class Listener<T extends Event> {
+  // abstract: must be defined by a sub-class
+
+  // subject: a channel/event-name to listen or subscribe to i.e ticket-created
+  abstract subject: T['subject']
+  abstract queueGroupName: string
+  abstract onMessage(data: T['data'], msg: Message): void
+
+  private client: Stan
+  protected ackWait = 5 * 1000
+
+  constructor(client: Stan) {
+    this.client = client
+  }
+
+  // create subscriptionOptions
+  subscriptionOptions() {
+    return this.client.subscriptionOptions()
+    .setDeliverAllAvailable()
+    .setManualAckMode(true)
+    .setAckWait(this.ackWait)
+    .setDurableName(this.queueGroupName)
+  }
+
+  listen() {
+    // create subscription
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    )
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received... ${this.subject} / ${this.queueGroupName}`)
+
+      const parsedData = this.parseMessage(msg)
+      this.onMessage(parsedData, msg)
+    })
+  }
+
+  parseMessage(msg: Message) {
+    const data =  msg.getData()
+
+    return typeof data === 'string'
+      ? data
+      : JSON.parse(data.toString('utf-8'))
+  }
+}
---
+++ src/events/base/base-publisher.ts
@@ -0,0 +1,28 @@
+import { Stan } from 'node-nats-streaming'
+import { Subjects } from '../subjects'
+
+interface Event {
+  subject: Subjects,
+  data: any,
+}
+export abstract class Publisher<T extends Event> {
+  // abstract: must be defined by a sub-class
+
+  // subject: a channel to publish an event to i.e ticket-created
+  abstract subject: T['subject']
+  private client: Stan
+
+  constructor(client: Stan) {
+    this.client = client
+  }
+
+  publish(data: T['data']): Promise<void> {
+    // publish an event with data
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        return err ? reject(err) 
+          : resolve(console.log( `Even published to subject: ${this.subject}`))
+      })
+    })
+  }
+}
