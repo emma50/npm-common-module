17b69f0b79a95876630f2ddf78cec47afc77cc69
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-09-23 13:18:28 +0100

    Updates

---
+++ src/events/order-event/order-cancel-event.ts
@@ -0,0 +1,15 @@
+import { Subjects } from "../subjects";
+import { OrderStatus } from "../types/order-status";
+
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled,
+  data: {
+    id: string,
+    status: OrderStatus,
+    userId: string,
+    ticket: {
+      id: string,
+      price: string
+    }
+  }
+}
