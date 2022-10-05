2c64daaa0bb699e482def5e337b1714f469bdb38
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2022-10-05 09:00:04 +0100

    Updates

--- src/events/base/base-listener.ts
+++ src/events/base/base-listener.ts
@@ -40,6 +40,7 @@ export abstract class Listener<T extends Event> {
     subscription.on('message', (msg: Message) => {
       console.log(`Message received... ${this.subject} / ${this.queueGroupName}`)
       const parsedData = this.parseMessage(msg)
+      console.log(parsedData)
       this.onMessage(parsedData, msg)
     })
   }
